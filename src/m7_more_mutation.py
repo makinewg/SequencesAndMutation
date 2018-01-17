f3f220a64f6461dfc4670470a51b7339b93fff0f
makinewg <makinewg@rose-hulman.edu>
2018-01-17 15:51:50 -0500

    Last thing on m8 is fcking me up man

--- src/m7_more_mutation.py
+++ src/m7_more_mutation.py
@@ -4,14 +4,14 @@ by changing the values of instance variables.
 
 Authors: David Mutchler, Amanda Stouder, Chandan Rupakheti, Katie Dion,
          Claude Anderson, Delvin Defoe, Curt Clifton, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and William Makinen.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
 
 def main():
-    run_test_RETURN_circle()
+    # run_test_RETURN_circle()
     run_test_MUTATE_circle()
 
 
@@ -169,7 +169,12 @@ def RETURN_circle(circle, color):
     where the color is either a string that Rosegraphics understands
     or a rg.Color object.
     """
-    # TODO: 2. First, READ THE ABOVE TEST CODE.
+
+    newcircle = rg.Circle(circle.center, circle.radius)
+    newcircle.fill_color = color
+    return newcircle
+
+    # DONE: 2. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
@@ -301,7 +306,11 @@ def MUTATE_circle(circle, color, n):
     Precondition: The first argument is a rg.Circle and the second
                   argument is a color appropriate for Zellegraphics.
     """
-    # TODO: 3. First, READ THE ABOVE TEST CODE.
+
+    circle.center = rg.Point(circle.center.x+n, circle.center.y)
+    circle.fill_color = color
+
+    # DONE: 3. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
